af84ce76d1c87144e0a1e1321b36f4d0f1bab850
Yosuke-Higuchi <higuchi@hi.is.kit.ac.jp>
2017-05-25 06:50:26 +0900

    socket_lib.py内に残していたserver.pyにおけるmain処理部分を削除

--- socket_lib.py
+++ socket_lib.py
@@ -1,6 +1,5 @@
 import sys, socket, select
 
-
 class ServerSocket:
     """Deal server sockert with not a main function but an object"""
     def __init__(self, port = 5001,  socklist = [], server = '127.0.0.1', serversock = None):
@@ -33,29 +32,3 @@ class ServerSocket:
                 except :
                     socket.close()
                     self.socklist.remove(socket)
-
-if __name__ == '__main__':
-    # port, socklist, server = 5001, [], '127.0.0.1' if len(sys.argv) <= 1 else sys.argv[1]
-    # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # server_socket.bind((server, port))
-    # server_socket.listen(10)
-    # socklist.append(server_socket)
-    print('Start')
-    while True:
-        read_sockets, write_sockets, error_sockets = select.select(socklist, [], [])
-        for sock in read_sockets:
-            if sock == server_socket:
-                sockfd, addr = server_socket.accept()
-                socklist.append(sockfd)
-                broadcast(socklist, server_socket, sockfd, '[%s:%s] Enter' % addr)
-            else:
-                try:
-                    data = sock.recv(4096).decode()
-                    if data == '': raise Exception('Done')
-                    if data:
-                        broadcast(socklist, server_socket, sock, data)
-                except Exception as e:
-                    print(e)
-                    broadcast(socklist, server_socket, sock, '[%s, %s] Exit' % addr)
-                    sock.close()
-                    socklist.remove(sock)
